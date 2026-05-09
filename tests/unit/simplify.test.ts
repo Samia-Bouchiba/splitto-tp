a813502b55f2c83d59116deb7e303f83133e1ec3
Samia-Bouchiba <samia.bouchiba.2001@gmail.com>
2026-05-09 13:12:53 +0200

    RED: ajout du test pour 3 personnes en triangle

--- tests/unit/simplify.test.ts
+++ tests/unit/simplify.test.ts
@@ -10,3 +10,9 @@ describe('simplifyDebts', () => {
   });
 
 });
+
+it('3 personnes : c doit 10€ à a, b est équilibré', () => {
+    const result = simplifyDebts({ a: 10, b: 0, c: -10 });
+    expect(result).toHaveLength(1);
+    expect(result[0]).toEqual({ from: 'c', to: 'a', amount: 10 });
+  });
